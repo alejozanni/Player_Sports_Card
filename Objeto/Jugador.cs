e4e3f4329b4420452215241b98ec746583838e31
Alejo Zanni <56770851+alejozanni@users.noreply.github.com>
2021-12-06 16:22:13 -0300

    Add files via upload

---
+++ Objeto/Jugador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalLab2
+{
+    public class Jugador : Persona
+    {      
+        private int _partidosJugados;
+        private int _partidosGanados;
+        private int _categoriaActual;
+        private int _torneosJugados;
+        private int _torneosGanados;
+ 
+        public Jugador(int partidosJugados, int partidosGanados, int categoriaActual, int torneosJugados, int torneosGanados)
+        {
+            _partidosJugados = partidosJugados;
+            _partidosGanados = partidosGanados;
+            _categoriaActual = categoriaActual;
+            _torneosJugados = torneosJugados;
+            _torneosGanados = torneosGanados;
+        }
+
+       
+        public Jugador(int id, string nombreCompleto, string DNI, int celular, string imagen, int partidosJugados, int partidosGanados, int categoriaActual,int torneosJugados, int torneosGanados)
+            :base (id, nombreCompleto, DNI, celular, imagen)
+        {
+            _partidosJugados = partidosJugados;
+            _partidosGanados = partidosGanados;
+            _categoriaActual = categoriaActual;
+            _torneosJugados = torneosJugados;
+            _torneosGanados = torneosGanados;
+        }
+
+        
+       public int PartidosJugados { get => _partidosJugados; set => _partidosJugados = value; }
+       public int PartidosGanados { get => _partidosGanados; set => _partidosGanados = value; }
+       public int CategoriaActual { get => _categoriaActual; set => _categoriaActual = value; }
+       public int TorneosJugados { get => _torneosJugados; set => _torneosJugados = value; }
+       public int TorneosGanados { get => _torneosGanados; set => _torneosGanados = value; }
+       
+    }
+}
---
+++ Objeto/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalLab2
+{
+    public abstract class Persona
+    {
+        private int _id;
+        private string _nombreCompleto;
+        private string _DNI;
+        private int _celular;
+        private string _imagen;
+        
+        public Persona() { }
+        public Persona(int id, string nombreCompleto, string DNI, int celular, string imagen)
+        {
+            _id = id;
+            _nombreCompleto = nombreCompleto;
+            _DNI = DNI;
+            _celular = celular;
+            _imagen = imagen;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string NombreCompleto { get => this._nombreCompleto; set => this._nombreCompleto = value; }
+        public string DNI { get => this._DNI; set => this._DNI = value; }
+        public int Celular { get => this._celular; set => this._celular = value; }
+        public string Imagen { get => _imagen; set => _imagen = value; }
+    }
+}
